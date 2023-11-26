27474dac588b5f67ef0bc7cb9ff9d1b275001f37
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-11-26 15:09:09 +0530

    mongoDb url updated

--- connectionToDB.js
+++ connectionToDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+
 
 mongoose
-  .connect(process.env.CONNECTION_URL)
+  .connect("mongodb+srv://Vishwa3121:Mongodb123@cluster0.oenylg3.mongodb.net/users?retryWrites=true&w=majority")
   .then(() => console.log("Connected to the database"))
   .catch((err) => console.log(err));
